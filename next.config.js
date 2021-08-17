9e0ea9e7448c56df774981a800ba900e31f8c24c
Izugarri <57817632+Izugarri@users.noreply.github.com>
2021-08-17 19:34:23 +0200

    search

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
-// This file was automatically added by xdn init.
-// You should commit this file to source control.
-
+module.exports = {
+  reactStrictMode: true,
+}
 const { withLayer0, withServiceWorker } = require('@layer0/next/config')
 
-module.exports = withLayer0(withServiceWorker())
+  module.exports = withLayer0(
+    withServiceWorker({
+      layer0SourceMaps: true,
+      // ...
+      // additional Next.js config options here
+      // ...
+      async rewrites() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+      },
+    ]
+  },
+    })
+  )
